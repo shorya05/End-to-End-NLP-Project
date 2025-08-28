7bd3d0a64d2a3b95dad986bfd5acffaa86544c53
Shorya Agarwal <shoryaaeran05@gmail.com>
2025-08-28 16:29:52 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="shorya",
+    author_email="shoryaaeran05@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
